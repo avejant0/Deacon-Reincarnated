d908800237cff330e396dfa7e9a9af49bfb30017
Maria Muravyova <avejant0@gmail.com>
2021-07-01 22:09:27 +0300

    No-issue: Installed typeorm packages and added Typeorm module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'deacon',
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
